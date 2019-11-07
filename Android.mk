36d59c613ee1d71e7718167c2a4b681fabe6d794
Jeremy Friesen <jeremy.friesen@thisisant.com>
2019-11-06 17:50:33 -0700

    Release candidate build 6.8.00
    Support for Android SDK 29

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@
 # 
 # ANT Stack
 # 
-# Copyright 2018 Dynastream Innovations
+# Copyright 2019 Garmin Canada Inc.
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
